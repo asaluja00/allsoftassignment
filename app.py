aebbc6a39b8c8f0141bb5818066f6c38e53a36f2
Anmol saluja <65105427+asaluja00@users.noreply.github.com>
2026-01-19 01:43:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask, jsonify
+import logging
 import os
 
 app = Flask(__name__)
 
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 @app.route("/hello", methods=["GET"])
 def hello():
-    return jsonify({"message": "Hello from Flask on Cloud Run"})
+    logger.info("Hello API was called")
+    return jsonify({"message": "Hello from Flask"})
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
